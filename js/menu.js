98df9076a54ee5e40b5ee952114eff53574b53f3
meliwi <melissa.gonzalez@correounivalle.edu.co>
2021-02-01 19:42:03 -0500

    header of page

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+//Almacenamos el div y el boton de menú en variable
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+
+//Añadimos un evento al boton menú
+btnMenu.addEventListener('click', function(){
+  nav.classList.toggle('show');
+});
